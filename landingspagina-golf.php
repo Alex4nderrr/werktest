ee325ab07dbacfcd012db3f0ab6e9c2e8aa0eac3
Alex4nderrr <alexander.wijnstekers@gmail.com>
2017-11-07 16:02:27 +0100

    Update landingspagina-golf.php

--- landingspagina-golf.php
+++ landingspagina-golf.php
@@ -1,1 +1,13 @@
+.G-content{width: 100%;position: absolute;z-index: 20;background-color: #f1f1f1;min-height: 100%;top: -23px;left: 0;background-size: contain;padding-bottom: 100px;}
+.G-content p{margin:0;}
+.G-background{width:100%;height:400px;position:absolute}
+.G-background > img {width: 100%; max-height: 400px;}
 
+.G-left{text-align: left !important;}
+.G-center{text-align: center !important;}
+.G-right{text-align: right !important;}
+
+body {overflow: scroll !important;}
+body::-webkit-scrollbar-track {background-color: #202020 !important;}
+body::-webkit-scrollbar {width: 10px !important; background-color: #ffffff !important;}
+body::-webkit-scrollbar-thumb {background-color: #ffffff !important;}
